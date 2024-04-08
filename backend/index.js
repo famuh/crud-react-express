5a3da5b6613fab29883967474c8cef6cb5a337de
famuh <famuh79@gmail.com>
2024-04-09 06:47:22 +0700

    CREATE backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import UserRoute from "./routes/UserRoute.js"
+
+const app = express();
+
+// midleware
+app.use(cors());
+app.use(express.json());
+app.use(UserRoute)
+
+app.listen(5000, ()=> console.log("App is running . . ."))
